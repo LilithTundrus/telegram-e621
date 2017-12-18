7c4159a454e5f7be60218940e94f35d9f5480ece
Lilith Tundrus <echofox123@gmail.com>
2017-12-17 23:37:11 -0500

    I fucking hate being serious about databases but aaaaaaaa

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,9 @@
 module.exports = {
     client: 'mysql',
     connection: {
-      user: 'root',
-      password: 'fortfairland!17',
-      database: 'node_test'
+        user: 'root',
+        password: 'fortfairland!17',
+        database: 'node_test'
     }
-  }
+}
+
