4ff10234553ae3f82946e0e15cf558d9fb010b66
Lilith Tundrus <echofox123@gmail.com>
2017-12-18 23:54:37 -0500

    added  config-template.js

---
+++ config/config-template.js
@@ -0,0 +1,23 @@
+exports.VER = 'BOT_VERSION';
+exports.USER_AGENT = `BOT_USER_AGENT`;
+exports.BOT_TOKEN = 'YOUR_BOT_TOKEN';
+
+exports.TELEGRAM_ADMIN_ID = 'YOUR_ADMIN_ID';
+exports.devContactName = 'DEVELOPER_NAME'
+
+exports.debugLogDir = './logs/debug.log';
+exports.infoLogDir = './logs/info.log';
+exports.authLogDir = './logs/auth.log';
+exports.warnLogDir = './logs/warnings.log';
+exports.errorLogDir = './logs/errors.log';
+
+
+exports.e621BaseUrl = 'https://e621.net/';
+exports.e621DefaultPageSize = 50;
+exports.e621DefaultPageLimit = 3;
+exports.linkDefaultLimit = 25;
+
+exports.dbUserName = 'USER';
+exports.dbPassword = 'PASSWORD';
+exports.dbName = 'NAME_OF_DB_TO_CONNECT_TO';
+exports.dbHost = 'HOST_ADDRESS';
