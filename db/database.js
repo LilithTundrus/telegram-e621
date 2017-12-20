cc63ef039c97ae373f9668a441bc9fa95405fc18
Lilith Tundrus <echofox123@gmail.com>
2017-12-19 19:05:25 -0500

    added basic DB middleware

--- db/database.js
+++ db/database.js
@@ -1,16 +1,22 @@
 'use strict';
 const mysql = require('mysql');
 const CONFIG = require('../config/config.js');
-
+const Logger = require('../lib/loggerClass.js');
+const logger = new Logger();                                          // Create an instance of our custom logger
 
 var con = mysql.createConnection({
-  host: CONFIG.dbHost,
-  user: CONFIG.dbUserName,
-  password: CONFIG.dbPassword
+    host: CONFIG.dbHost,
+    user: CONFIG.dbUserName,
+    password: CONFIG.dbPassword
 });
 
+function connect() {
+    return con.connect(function (err) {
+        if (err) throw err;
+        logger.info('Connected to DB.');
+    });
+}
 
-con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+//put the middleware here!
+
+module.exports.connect = connect;
