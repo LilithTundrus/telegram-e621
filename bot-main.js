e2f1c963c655bfca77d82708eb90eb3c1601fd0b
Lilith Tundrus <echofox123@gmail.com>
2017-12-20 20:30:11 -0500

    testing wizard stuff

--- bot-main.js
+++ bot-main.js
@@ -51,8 +51,48 @@ Notes:
 */
 logger.info(`e621client_bot ${VER} started at: ${new Date().toISOString()}`);
 db.connect();
-app.startPolling();                                                 // start the bot and keep listening for events
 
+
+
+
+const stepHandler = new Composer()
+stepHandler.action('next', (ctx) => {
+    ctx.reply('Step 2. Via inline button')
+    return ctx.wizard.next()
+})
+stepHandler.command('next', (ctx) => {
+    ctx.reply('Step 2. Via command')
+    return ctx.wizard.next()
+})
+stepHandler.use((ctx) => ctx.replyWithMarkdown('Press `Next` button or type /next'))
+
+const superWizard = new WizardScene('super-wizard',
+    (ctx) => {
+        ctx.reply('Step 1', Markup.inlineKeyboard([
+            Markup.urlButton('❤️', 'http://telegraf.js.org'),
+            Markup.callbackButton('➡️ Next', 'next')
+        ]).extra())
+        return ctx.wizard.next()
+    },
+    stepHandler,
+    (ctx) => {
+        ctx.reply('Step 3')
+        return ctx.wizard.next()
+    },
+    (ctx) => {
+        ctx.reply('Step 4')
+        return ctx.wizard.next()
+    },
+    (ctx) => {
+        ctx.reply('Done')
+        return ctx.scene.leave()
+    }
+)
+
+const stage = new Stage([superWizard], { default: 'super-wizard' });
+app.startPolling();                                                 // start the bot and keep listening for events
+app.use(session());
+app.use(stage.middleware());
 // #region appCommands
 app.command('start', ({ from, reply }) => {
     logger.info(`Start from ${JSON.stringify(from)}`);              // log when a new user starts the bot
@@ -110,6 +150,13 @@ app.command('custom', ({ reply }) => {
     )
 });
 // #endregion
+
+
+
+
+
+
+
 
 //TODO: allow this to show all results through pagination
 function popularSearchHandler(teleCtx, typeArg) {
