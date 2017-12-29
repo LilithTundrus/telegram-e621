cbdf7a3be2edf6257c51f7ee0c4dfba67b151375
Lilith Tundrus <echofox123@gmail.com>
2017-12-29 00:26:19 -0500

    added processInfoHandler

--- bot/commands/index.js
+++ bot/commands/index.js
@@ -1,15 +1,14 @@
 //put all of the commands together!!
 'use strict';
-
 const { Composer } = require('telegraf');
 const composer = new Composer();
 
+// Require the set of commands to use
 const startHandler = require('./startHandler');
+const processInfoHandler = require('./processInfoHandler');
 
-
-
-
+// Assign the command listeners to the required components from above
 composer.command('start', startHandler);
-
+composer.command('processinfo', processInfoHandler);
 
 module.exports = composer;
