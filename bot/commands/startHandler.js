143977df1260178275c7e29744cef2bbd8c741c0
Lilith Tundrus <echofox123@gmail.com>
2017-12-29 00:20:02 -0500

    Minor formatting fixes

--- bot/commands/startHandler.js
+++ bot/commands/startHandler.js
@@ -1,9 +1,9 @@
 'use strict';
-const Logger = require('../../lib/loggerClass');                        // Our custom logging class
+const Logger = require('../../lib/loggerClass');                    // Our custom logging class
 const logger = new Logger();                                        // Create an instance of our custom logger
 
 const startHandler = async (ctx) => {
-    logger.info(`Start from ${JSON.stringify(ctx.message.from)}`);              // log when a new user starts the bot
+    logger.info(`Start from ${JSON.stringify(ctx.message.from)}`);  // log when a new user starts the bot
     return ctx.reply('Henlo! ${WELCOME_MESSAGE_HERE}');
 }
 
