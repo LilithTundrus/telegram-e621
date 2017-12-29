0f97febb4561fb6b7812f2e8bf0afe312ddca057
Lilith Tundrus <echofox123@gmail.com>
2017-12-28 21:14:03 -0500

    minimum bot stuff working!

--- index.js
+++ index.js
@@ -4,4 +4,23 @@
  * @type {Telegraf}
  * Bot
  */
-const bot = require('./bot');
+const bot = require('./bot/bot-main');
+const errHandler = require('./utils/botErrHandler');
+const config = require('./config/config');
+const Logger = require('./lib/loggerClass.js');                     // Our custom logging class
+const logger = new Logger();                                        // Create an instance of our custom logger
+
+bot.telegram.getMe().then((botInfo) => {
+    bot.options.username = botInfo.username;
+});
+
+// Put middleware stuff here
+bot.use();
+
+
+// Start the bot
+bot.startPolling();
+logger.info(`e621client_bot ${config.VER} started at: ${new Date().toISOString()}`);
+
+
+bot.catch(errHandler);
