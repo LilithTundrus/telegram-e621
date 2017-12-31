4bfd719333339a729c602c08785831c3dc51bbd3
Lilith Tundrus <echofox123@gmail.com>
2017-12-30 19:31:08 -0500

    added the current keyboard to this module on start so /menu doesn't have to be used

--- bot/commands/startHandler.js
+++ bot/commands/startHandler.js
@@ -1,10 +1,20 @@
 'use strict';
 const Logger = require('../../lib/loggerClass');                    // Our custom logging class
 const logger = new Logger();                                        // Create an instance of our custom logger
+const Markup = require('telegraf/markup');
+
 
 const startHandler = async (ctx) => {
     logger.info(`Start from ${JSON.stringify(ctx.message.from)}`);  // log when a new user starts the bot
-    return ctx.reply('Henlo! ${WELCOME_MESSAGE_HERE}');
+    return ctx.reply('Henlo! ${WELCOME_MESSAGE_HERE}', Markup
+        .keyboard([
+            ['🔍 Search', '😎 Popular'],
+            ['☸ Settings', '📞 Feedback'],
+        ])
+        .oneTime()
+        .resize()
+        .extra()
+    );
 }
 
 module.exports = startHandler;
